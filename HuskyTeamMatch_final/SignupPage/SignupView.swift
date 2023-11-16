ad0ec83eea5b7503f223091ab775316655a7c3df
TianMa <jwang268@dons.usfca.edu>
2023-11-16 11:24:27 -0800

    add sign up page

---
+++ HuskyTeamMatch_final/SignupPage/SignupView.swift
@@ -0,0 +1,20 @@
+//
+//  SignupView.swift
+//  HuskyTeamMatch_final
+//
+//  Created by Tian Ma on 11/16/23.
+//
+
+import UIKit
+
+class SignupView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
