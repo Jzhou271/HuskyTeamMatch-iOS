2c865cfdc767a715a5829884deade65e6a8c7408
Junjia Wang <jwang268@dons.usfca.edu>
2023-11-16 10:29:28 -0800

    Initial Commit

---
+++ HuskyTeamMatch_final/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HuskyTeamMatch_final
+//
+//  Created by junjia wang on 11/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
