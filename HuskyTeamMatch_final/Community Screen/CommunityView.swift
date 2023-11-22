a41174307edbe7fcc9ba56a14b5b2715bd665ba7
Jing Zhou <ivy_qq@live.cn>
2023-11-21 21:48:32 -0800

    Resolved merge conflicts

--- HuskyTeamMatch_final/Community Screen/CommunityView.swift
+++ HuskyTeamMatch_final/Community Screen/CommunityView.swift
@@ -6,15 +6,40 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class CommunityView: UIView {
+    
+    var greetingLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = .white
+        let displayName = Auth.auth().currentUser?.displayName
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+        setupGreetingLabel(name: displayName)
+        initConstraints()
+        
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func setupGreetingLabel(name: String?) {
+        greetingLabel = UILabel()
+        greetingLabel.text = "Welcome, \(name ?? "")!"
+        greetingLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(greetingLabel)
+    }
+    
+    
+    func initConstraints(){
+        NSLayoutConstraint.activate([
+            greetingLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 150),
+            greetingLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40)
+        ])
+    }
 }
--- HuskyTeamMatch_final/Community Screen/CommunityViewController.swift
+++ HuskyTeamMatch_final/Community Screen/CommunityViewController.swift
@@ -6,25 +6,40 @@
 //
 
 import UIKit
+import FirebaseAuth
+
 
 class CommunityViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        self.title = "Community"
+    
+    let communityView = CommunityView()
+    
+    override func loadView() {
+        view = communityView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Community"
+        
+        let logoutButtonItem = UIBarButtonItem(title: "Log Out", style: .plain, target: self, action: #selector(onLogoutButtonTapped))
+        self.navigationItem.rightBarButtonItem = logoutButtonItem
     }
-    */
+    
+    @objc func onLogoutButtonTapped() {
+        do {
+            try Auth.auth().signOut()
+
+            let viewController = SigninViewController()
+            let navController = UINavigationController(rootViewController: viewController)
+
+            if let window = UIApplication.shared.windows.first {
+                window.rootViewController = navController
+                window.makeKeyAndVisible()
+            }
+            
+        } catch let signOutError as NSError {
+            print("Error signing out: \(signOutError)")
+        }
+    }
 
 }
